789d329fbc0f8075790ff4b0fcd4f145f5c27250
Pablo Mocholi <pbmocholi_98@hotmail.es>
2021-02-10 18:46:40 +0100

    Ejercicio 5

--- app/src/main/java/pabmocpl/dadm/labs/myquote/QuotationActivity.java
+++ app/src/main/java/pabmocpl/dadm/labs/myquote/QuotationActivity.java
@@ -3,6 +3,10 @@ package pabmocpl.dadm.labs.myquote;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class QuotationActivity extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class QuotationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quotation);
+
+        TextView tvQuotation = findViewById(R.id.tvQuotation);
+        String data = tvQuotation.getText().toString().replaceAll("%1s", getString(R.string.sample_name));
+        tvQuotation.setText(data);
+
+        TextView tvAuthor = findViewById(R.id.tvAuthor);
+        ImageButton refreshButton = findViewById(R.id.ibRefresh);
+        refreshButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvQuotation.setText(R.string.sample_quote);
+                tvAuthor.setText(R.string.sample_author);
+            }
+        });
     }
 }
